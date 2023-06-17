f1c6f38460b1e66f80361e21669327a6d2aaebb8
Skjold <sk.dyre@gmail.com>
2023-06-17 20:14:46 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
+import { ErrorComponentComponent } from './components/utilities/error-component/error-component.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomepageComponent},
+  { path: '**', component: ErrorComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage/homepage.component';
+import { ProfileHomeComponent } from './components/profile/profileHome/profile-home/profile-home.component';
+import { ErrorComponentComponent } from './components/utilities/error-component/error-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProfileHomeComponent,
+    ErrorComponentComponent
   ],
   imports: [
     BrowserModule,
