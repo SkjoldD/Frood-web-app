fa80adbda50af0a344f7a76b667bf38ae8ed6a50
Merle Dyre <medy@itu.dk>
2023-07-05 12:40:07 +0200

    homepage 50% done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage/homepage.component';
 import { ErrorComponentComponent } from './components/utilities/error-component/error-component.component';
+import { LoginpageComponent } from './components/loginpage/loginpage/loginpage.component';
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent},
+  { path: 'login', component: LoginpageComponent},
   { path: '**', component: ErrorComponentComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage/homepage.component';
 import { ProfileHomeComponent } from './components/profile/profileHome/profile-home/profile-home.component';
 import { ErrorComponentComponent } from './components/utilities/error-component/error-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginpageComponent } from './components/loginpage/loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     ProfileHomeComponent,
-    ErrorComponentComponent
+    ErrorComponentComponent,
+    LoginpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatDividerModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
