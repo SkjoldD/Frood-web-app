0ef75d55c861eb2c3c88542fee1ffdaf8fa5a216
Skjold <sk.dyre@gmail.com>
2023-07-09 18:43:16 +0200

    Added map, in development

--- src/app/components/startingpage/startingpage.component.ts
+++ src/app/components/startingpage/startingpage.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface MarkerProperties {
+  position: {
+    lat: number;
+    lng: number;
+  }
+};
 
 @Component({
   selector: 'app-startingpage',
   templateUrl: './startingpage.component.html',
   styleUrls: ['./startingpage.component.css']
 })
-export class StartingpageComponent {
+
+export class StartingpageComponent implements OnInit {
+
+    zoom = 12;
+    center: google.maps.LatLngLiteral  = {lat: 55.6638295, lng: 12.5414299};
+
+    markers: MarkerProperties[] = [
+      { position: { lat: 48.8584, lng: 2.2945 }}, // Eiffel Tower
+    ];
+
+    options: google.maps.MapOptions = {
+      zoomControl: false,
+      scrollwheel: true,
+      disableDoubleClickZoom: true,
+      maxZoom: 24,
+      minZoom: 4,
+      disableDefaultUI: true
+    };
+
+    ngOnInit() {
+      
+      return this.geogeo();
+    }
+
+    geogeo(){
+      navigator.geolocation.getCurrentPosition((position) => {
+
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        
+        this.markers.push({ position: { lat: this.center.lat, lng: this.center.lng }})
+
+      }, function (e) {
+        //Your error handling here
+        console.log("Enable location");
+      }, {
+          timeout: 2000,
+          enableHighAccuracy: true
+      });
+    }
+
+    
 
 }
