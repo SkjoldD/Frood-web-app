c26a41354abde31c184f4cd475d1148eae510f43
Merle Dyre <medy@itu.dk>
2023-07-09 16:58:22 +0200

    signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { StartingpageComponent } from './components/startingpage/startingpage.component';
 import { ProfileHomeComponent } from './components/profile/profileHome/profile-home/profile-home.component';
 import { ErrorComponentComponent } from './components/utilities/error-component/error-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,12 +14,14 @@ import {Component} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
-import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import {LoginpageComponent} from './components/loginpage/loginpage.component';
 import {MatInputModule} from '@angular/material/input'; 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
-import { SignupComponent } from './components/signup/signup.component';
-import { StartingpageComponent } from './components/startingpage/startingpage.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
@@ -38,7 +42,9 @@ import { StartingpageComponent } from './components/startingpage/startingpage.co
     MatIconModule,
     MatInputModule,
     FormsModule, 
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDatepickerModule, 
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
